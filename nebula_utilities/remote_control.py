3b916047e511ad365a7cfe0c06733857ffdf0986
Marc Pernia <marc.pernia@redaptiveinc.com>
2019-09-29 21:52:18 -0700

    file_transfer() Added support for transferring a list of files. Added support for transferring file-like objects. Added simple examples of the above to main().

--- nebula_utilities/remote_control.py
+++ nebula_utilities/remote_control.py
@@ -5,7 +5,9 @@ __version__ = '0.1'
 __version_info__ = (0, 1)
 __author__ = 'SuicidalLabRat <suicidallabrat@gmail.com>'
 import sys
+from os import path
 from time import sleep
+from io import StringIO
 from getpass import getpass
 
 # Import 3rd party modules
@@ -120,17 +122,19 @@ def root_ssh_access(hname, uname, passwd, rpasswd=None, enable=False, port=22, t
         return False, None
 
 
-def file_transfer(hname, uname, passwd, local_path, remote_path, put_file=True, port=22):
+def file_transfer(hname, uname, passwd, file_paths,  put_file=True, port=22):
     """
     Transfer a file to or from a meter.
     :param (str) hname: The IP address or hostname of the target meter.
     :param (str) uname: User account on the target.
     :param (str) passwd: Password for the given target meters user account.
-    :param (str) local_path: Path and filename of local file to be transferred to the target meter.
-    :param (str) remote_path: Absolute path and filename where the source file will be written on the target meter.
+    :param (list) file_paths: List of tuples containing source filename, including path, as a string OR file-like-object
+     and the destination filename, including path, as a strings.
     :param (bool) put_file: If true, send a file to the target meter, otherwise download a file from the target meter.
     :param (int) port: ssh server port, default = 22.
-    :return (bool):
+    :return (bool, list): Returns true if all file transfers are successful, otherwise false.  Also returns a list of
+    key: bool, value: tuple, where tuple is the src and dst files and the key represents the success/failure of that
+    particular file transfer.
     """
     with SSHClient() as ssh_client:
         ssh_client.set_missing_host_key_policy(AutoAddPolicy())
@@ -143,29 +147,55 @@ def file_transfer(hname, uname, passwd, local_path, remote_path, put_file=True, 
             print(f'!!! ConnectionError: Unable to connect to port 22 on {hname} !!!\n')
             return False
         else:
+            return_status = True
+            status_list = []
             with SCPClient(ssh_client.get_transport()) as scp:
-                if put_file:
-                    try:
-                        scp.put(local_path, remote_path)
-                    except SCPException:
-                        print(f'!!! FileTransferError: Scp exception trying to transfer file !!!\n')
-                        return False
-                    except IOError:
-                        print(f'!!! IOError: Error accessing {local_path} !!!')
-                        return False
-                    else:
-                        return True
 
-                try:
-                    scp.get(remote_path)
-                except SCPException:
-                    print(f'!!! FileTransferError: Scp exception trying to get remote file {remote_path} !!!\n')
-                    return False
-                except IOError:
-                    print(f'!!! IOError: Error writing remote file {remote_path} locally !!!\n')
-                    return False
-                else:
-                    return True
+                if put_file:
+                    for src, dst in file_paths:
+                        if not isinstance(src, StringIO) and not path.isfile(src):
+                            print(f'!!! IOError: Error accessing {src} !!!\n')
+                            status_list.append((False, (src, dst)))
+                            return_status = False
+                            continue
+                        try:
+                            if not isinstance(src, StringIO):
+                                scp.put(src, dst)
+                            else:
+                                print(f'src is a file-like-object!')
+                                scp.putfo(src, dst)
+
+                        except SCPException:
+                            print(f'!!! FileTransferError: Scp exception trying to transfer file {src} -> {dst} !!!\n')
+                            status_list.append((False, (src, dst)))
+                            return_status = False
+                        except IOError:
+                            print(f'!!! IOError: Error accessing {src} !!!\n')
+                            status_list.append((False, (src, dst)))
+                            return_status = False
+                        except ssh_exception.SSHException:
+                            print('ssh exception!')
+
+                        else:
+                            status_list.append((True, (src, dst)))
+
+                    return return_status, status_list
+
+                for src, dst in file_paths:
+                    try:
+                        scp.get(dst)
+                    except SCPException:
+                        print(f'!!! FileTransferError: Scp exception trying to get remote file {dst} !!!\n')
+                        status_list.append((False, (src, dst)))
+                        return_status = False
+                    except IOError:
+                        print(f'!!! IOError: Error writing remote file {dst} locally !!!\n')
+                        status_list.append((False, (src, dst)))
+                        return_status = False
+                    else:
+                        status_list.append((True, (src, dst)))
+
+                return return_status, status_list
 
 
 def main():
@@ -173,13 +203,27 @@ def main():
     Usage example...
     """
     # We need to provide some basic info to describe the target meter.
-    hostname = '192.168.0.1'
+    hostname = '192.168.0.186'
     username = 'redaptive'
     root_username = 'root'
-    password = getpass(prompt=f'Enter password for the \'{username}\' user: ')
-    root_password = getpass(prompt=f'Enter password for the \"{root_username}\" user: ')
-    local_file_path = 'meterCalData.json'
-    remote_file_path = '/data/redaptive/config'
+    password = 'xXxXxXxXxXxXx'  # getpass(prompt=f'Enter password for the \'{username}\' user: ')
+    root_password = 'test'  # getpass(prompt=f'Enter password for the \"{root_username}\" user: ')
+    cal_file_src = '../meterCalData.json'
+    cal_file_dst = '/data/'
+    config_file_src = '../meterConfigData.json'
+    config_file_dst = '/data/'
+    fl_name = '/data/fileLikeObject.txt'
+
+    # generate in-memory file-like object for testing.
+    fl = StringIO()
+    fl.write('This is supposed to be a file file object!\n\r')
+    fl.seek(0)
+
+    file_list = [
+        (cal_file_src, cal_file_dst),
+        (config_file_src, config_file_dst),
+        (fl, fl_name)
+    ]
 
     # Example enabling root ssh access on a given meter.
     # Note: Returns a bool representing whether the meters root ssh access is in the requested state.
@@ -187,11 +231,13 @@ def main():
     #       failed to get the meters current ssh configuration.
     (ssh_result, resulting_config) = root_ssh_access(hostname, username, password, root_password, True)
 
+    if ssh_result:
+        # Transfer a file to a remote meter - assumes root ssh access has been enabled on the remote meter.
+        scp_result, return_list = file_transfer(hostname, root_username, root_password, file_list)
+        print(f'Status: {scp_result}\nStatus list:\n{return_list}')
+
     # Example disabling root ssh access on a given meter.
     (ssh_result, resulting_config) = root_ssh_access(hostname, username, password, root_password)
-
-    # Transfer a file to a remote meter - assumes root ssh access has been enabled on the remote meter.
-    scp_result = file_transfer(hostname, root_username, root_password, local_file_path, remote_file_path)
 
 
 if __name__ == '__main__':
