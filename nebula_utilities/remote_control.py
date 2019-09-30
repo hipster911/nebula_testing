e62b27f9a86abf44f7404bbacf3f3fff2b35316b
Marc Pernia <marc.pernia@redaptiveinc.com>
2019-09-30 11:23:45 -0700

    purge secrets

--- nebula_utilities/remote_control.py
+++ nebula_utilities/remote_control.py
@@ -206,8 +206,8 @@ def main():
     hostname = '192.168.0.186'
     username = 'redaptive'
     root_username = 'root'
-    password = 'xXxXxXxXxXxXx'  # getpass(prompt=f'Enter password for the \'{username}\' user: ')
-    root_password = 'test'  # getpass(prompt=f'Enter password for the \"{root_username}\" user: ')
+    password = getpass(prompt=f'Enter password for the \'{username}\' user: ')
+    root_password = getpass(prompt=f'Enter password for the \"{root_username}\" user: ')
     cal_file_src = '../meterCalData.json'
     cal_file_dst = '/data/'
     config_file_src = '../meterConfigData.json'
