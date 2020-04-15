77f1ea1311cb06fb65569a488ed0fbfb6f30249a
Marc Pernia <marc.pernia@redaptiveinc.com>
2020-04-15 14:15:11 -0700

    Added radio band and cell tower TAC data to the output Status json object. Note: Lots of work was done and not committed, so I dont know exactly what has changed since last commit.

--- nebula_utilities/meter_status.py
+++ nebula_utilities/meter_status.py
@@ -9,8 +9,12 @@ import subprocess
 import socket
 import fcntl
 import struct
+import hashlib
+from platform import release as kernel_release
 from json import dumps
+import pathlib
 import csv
+
 
 def read_run_file(name):
     run_path = '/run/'
@@ -43,30 +47,122 @@ def get_software_version(name):
     elif re.search(r'mender(.*)', name, flags=re.IGNORECASE):
         v = run_cmd(['mender', '-version'])
         if v:
-            return v[0].split()[0].decode().strip()
+            arr = v[0].strip().split()
+
+            return arr[len(arr) - 3].decode().strip()
         else:
             return None
 
 
-def get_checksum(name):
-    # try to get the path from ps and get a checksum for the file.
-    pass
+def get_software_checksum(file, algorithm='MD5'):
+    """
+    Calculates the checksum of (a path to) a file, with the provided algorithm.
+    :param file: The input file for which a hash will be generated.
+    :param algorithm: Hashing algorithm - MD5|SHA-1|SHA-224|SHA-256|SHA-384|SHA-256.
+    :return: Tuple: [0]calculated checksum [1]algorithm used [2]file calculation is based on. [3]size of file (in bytes).
+    """
+    if algorithm == "MD5":
+        hash_func = hashlib.md5()
+    elif algorithm == "SHA-1":
+        hash_func = hashlib.sha1()
+    elif algorithm == "SHA-224":
+        hash_func = hashlib.sha224()
+    elif algorithm == "SHA-256":
+        hash_func = hashlib.sha256()
+    elif algorithm == "SHA-384":
+        hash_func = hashlib.sha384()
+    elif algorithm == "SHA-512":
+        hash_func = hashlib.sha512()
+    else:
+        print("Error: " + algorithm + " is an unknown algorithm!\n")
+        return None
 
-def get_pid( name):
-    # maybe use first five characters of name, case insensative, to get the pid and also derive the file path for checksums.
-    # /bin/ps | /bin/grep -i '.*/[i]ms2_cmd_interface.py'  use eithshell=true for pipes?
-    # /bin/ps | /bin/grep -i '.*/[r]d_accum'
-    # /bin/ps | /bin/grep -i '.*[u]ploader'
-    pass
+    # Read the file (in chunks; to prevent choking the RAM when reading large files).
+    try:
+        with open(file, "rb") as f:
+            while True:
+                file_data = f.read(8192)  # Block size = 8192 Bytes (8 KB)
+                if not file_data:
+                    break
+                hash_func.update(file_data)
+    except Exception as e:
+        print('Error calculating checksum fo file {0}!\n{1}'.format(file, e))
+        return None
+    else:
+        return hash_func.hexdigest(), \
+               algorithm, \
+               file, \
+               os.path.getsize(file)
 
-def get_ps_data(name):
-    # maybe use first five characters of name, case insensative, to get the pid and also derive the file path for checksums.
-    # /bin/ps | /bin/grep -i '.*/[i]ms2_cmd_interface.py'  use eithshell=true for pipes?
-    # /bin/ps | /bin/grep -i '.*/[r]d_accum'
-    # /bin/ps | /bin/grep -i '.*[u]ploader'
-    pid = ''
-    path = ''
-    return pid, path
+
+def get_eeprom_data():
+    # shell version of grabbing our hardware id data stored at i2c bus 0, chip address 0x50, data bytes 0x20-0x22:
+    # Set the pointer at the bit you want
+    # i2cset -y 0 0x50 0x20 0x20
+    # Get the data at that location, and the adjacent 2 bytes as well.
+    # i2cget -y 0 0x50     # Returns the byte at the current pointer, set above, and advances the pointer 1, to 0x21.
+    # i2cget -y 0 0x50     # Returns the byte at the current pointer, set above, and advances the pointer 1, to 0x22.
+    # i2cget -y 0 0x50     # Returns the byte at the current pointer, set above, and advances the pointer 1, to 0x23.
+
+    from smbus2 import SMBus
+    byte_count = 0
+    hardware_id = {}
+    address = 0x50
+    bus = smbus.SMBus(0)
+    bus.write_byte_data(address, 0x20, 0x20)  # the 0x20, 0x20 is the '16 bit' address split into 2 bytes
+    while byte_count <= 3:
+        try:
+            result = bus.read_byte(address)  # reads at the current address pointer, which we set on the previous line
+        except Exception as e:
+            print('Error getting hardware id from eeprom.')
+            return None
+        else:
+            byte_count += 1
+            if byte_count == 1:
+                hardware_id['model'] = result
+            elif byte_count == 2:
+                if hardware_id['model'] != '00':
+                    hardware_id['hsm'] = result
+                else:
+                    hardware_id['hsm'] = None
+                    hardware_id['version'] = result
+            elif byte_count == 3:
+                hardware_id['version'] = result
+    print(hardware_id)
+    return hardware_id
+
+
+def get_running_processes():
+    processes = []
+    pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+    for pid in pids:
+        try:
+            process = open(os.path.join('/proc', pid, 'cmdline'), 'r').read().split('\0')
+        except IOError:
+            print('Process ID {} is no longer there, maybe it has already terminated.'.format(pid))
+            continue
+        except Exception:
+            return None
+        else:
+            if not '' in process[:1]:
+                process.insert(0, pid)
+                processes.append(process)
+    if processes: return processes
+
+
+def get_process_info(proc_name):
+    """
+    Returns tuple with the process ID and the path/filename of the process executable.
+    If there are multiple instances of the given process name, only the first is returned.
+    """
+    processes = get_running_processes()
+    if processes:
+        for process in processes:
+            pid = process[0]
+            path_index = [n for n, x in enumerate(process) if proc_name in x]
+            if path_index:
+                return pid, process[path_index[0]] # If there are multiple instances, only return the first.
+    return None
 
 
 def run_cmd(*args, **kwargs):
@@ -137,6 +233,22 @@ def import_python_modules_by_path(path):
     return None
 
 
+def get_kernel_version():
+    try:
+        return kernel_release()
+    except Exception:
+        return None
+
+
+def get_uboot_version():
+    try:
+        version = subprocess.check_output('/sbin/fw_printenv | grep ver=U', shell=True).strip().decode()
+        return version.split('=')[1]
+    except Exception as e:
+        # print('Failed to get uboot version string.\n{}'.format(e))
+        return None
+
+
 def get_hostname():
     try:
         return socket.gethostname()
@@ -153,7 +265,7 @@ def get_system_time():
         return None
 
 
-def get_uptime():
+def get_uptime(fancy = False):
     try:
         with open('/proc/uptime', 'r') as f:
             uptime_seconds = float(f.readline().split()[0])
@@ -161,7 +273,9 @@ def get_uptime():
         print('failed to get system uptime')
         return None
     else:
-        return normalize_seconds(round(uptime_seconds))
+        if fancy:
+            return normalize_seconds(round(uptime_seconds))
+        return uptime_seconds
 
 
 def normalize_seconds(seconds: int) -> tuple or None:
@@ -206,7 +320,7 @@ def get_signal_str(path = '/var/spool/redaptive'):
         with open(os.path.join(path, latest), 'r') as f:
             s_str = f.readline().split(',')
     except Exception as e:
-        print(e)
+        # print(e)
         return None
     else:
         return s_str
@@ -229,7 +343,7 @@ def get_ip_address(ifname = 'eth0'):
             struct.pack('256s', ifname.encode()[:15])
         )[20:24])
     except Exception:
-        print('Failed to get ip address for interface {}.'.format(ifname))
+        # print('Failed to get ip address for interface {}.'.format(ifname))
         return None
     else:
         return ip_addr
@@ -257,7 +371,36 @@ def get_def_route():
     return None, stderr.decode('utf-8')
 
 
-def get_packet_loss(interface='ppp0', samples='1', hostname='8.8.8.8'):
+def get_net_traffic(t, iface='ppp0'):
+    """Example usage for throughput
+    while(True):
+    tx1 = get_bytes('tx')
+    rx1 = get_bytes('rx')
+
+    time.sleep(1)
+
+    tx2 = get_bytes('tx')
+    rx2 = get_bytes('rx')
+
+    tx_speed = round((tx2 - tx1)/1000000.0, 4)
+    rx_speed = round((rx2 - rx1)/1000000.0, 4)
+
+    print("TX: %fMbps  RX: %fMbps") % (tx_speed, rx_speed)
+    """
+    # Maybe we can return a total, and some stats, such as bytes per n time, say day and month.
+    uptime = round(get_uptime())
+    if uptime and uptime > 86400:
+        uptime_day = (uptime%86400)
+    try:
+        with open('/sys/class/net/' + iface + '/statistics/' + t + '_bytes', 'r') as f:
+            data = f.read()
+            return int(data)
+    except IOError as e:
+        print('{0} interface doesnt seem to exist.\n{1}'.format(iface, e))
+        return None
+
+
+def get_packet_loss(interface='eth0', samples='1', hostname='8.8.8.8'):
     ping = '/bin/ping'
     cmd = [ping, '-I', interface, '-c', samples, hostname]
     stdout, stderr = run_cmd(cmd)
@@ -337,6 +480,7 @@ def csv_header_from_nested_dict(dictionary):
     else:
         return header
 
+
 def csv_row_from_nested_dict(dictionary):
     row = dict()
     try:
@@ -353,7 +497,17 @@ def csv_row_from_nested_dict(dictionary):
         return row
 
 
+#class NebulaMeter(object):
+
+
 if __name__ == '__main__':
+    '''
+    We still need any important config info
+    Critical errors, if possible.
+    .
+    .
+    .
+    '''
     meter = {
         'mac_addr': get_mac(),
         'system': {},
@@ -364,7 +518,7 @@ if __name__ == '__main__':
 
     meter['system']['hostname'] = get_hostname()
     meter['system']['system_time'] = str(get_system_time())
-    meter['system']['uptime'] =  '{0.days}:{0.hours}:{0.minutes}:{0.seconds}'.format(get_uptime())
+    meter['system']['uptime'] =  '{0.days}:{0.hours}:{0.minutes}:{0.seconds}'.format(get_uptime(fancy=True))
     meter['system']['loadavg'] = get_load_average(1)
     meter['system']['mem_used'] = get_mem_percent_used()
 
@@ -379,39 +533,50 @@ if __name__ == '__main__':
     meter['networking']['ip_addr_ppp0'] = get_ip_address('ppp0')
     meter['networking']['ip_addr_active'] = get_active_ip_address()
     def_iface = get_def_route()[0]
-    meter['networking']['default_iface'] = def_iface[len(def_iface) - 1]
-    meter['networking']['iccid'] = 'NA'
-    meter['networking']['apn'] = 'NA'
+    meter['networking']['default_iface'] = def_iface[len(def_iface) - 1] if def_iface else None
+    meter['networking']['iccid'] = None
+    meter['networking']['apn'] = None
     sig_info = get_signal_str()
-    if sig_info:
-        meter['networking']['sig_str'] = sig_info[1]
-        meter['networking']['sig_qual'] = sig_info[2]
-    meter['networking']['packet_loss'] = get_packet_loss()
-    meter['networking']['network_errors'] = ''
+    meter['networking']['sig_str'] = sig_info[1] if sig_info else None
+    meter['networking']['sig_qual'] = sig_info[2] if sig_info else None
+    meter['networking']['cell_band'] = sig_info[3] if sig_info and len(sig_info) >= 4 else None
+    meter['networking']['cell_tac'] = sig_info[4] if sig_info and len(sig_info) >= 5 else None
+    meter['networking']['packet_loss'] = get_packet_loss(meter['networking']['ip_addr_active'])
+    meter['networking']['network_errors'] = None
 
+    ''' !!! WE NEED TO ADD PID's FOR IMPORTANT PROCESSES, SO WE CAN FLAG HIGH PID COUNTS. !!!'''
     meter['software']['redrock_os'] = get_os_version()
-    meter['software']['u-boot'] = 'NA'
-    meter['software']['kernel'] = 'NA'
+    meter['software']['u-boot'] = get_uboot_version()
+    meter['software']['kernel'] = get_kernel_version()
     meter['software']['stm32'] = get_software_version('stm32')
     meter['software']['meter-app'] = get_software_version('meter-app')
     meter['software']['rd-accum'] = get_software_version('rd-accum')
-    meter['software']['network-manager'] = 'NA'
-    meter['software']['upload-manager'] = 'NA'
-    meter['software']['led-controller'] = 'NA'
+    meter['software']['network-manager'] = get_software_checksum(get_process_info('network-manager')[1])[0]
+    meter['software']['upload-manager'] = get_software_checksum(importlib.util.find_spec('rdp_upman.uploader').origin)[0]
+    meter['software']['led-controller'] = get_software_checksum(get_process_info('led_controller')[1])[0]
     meter['software']['ims2-cmd-interface'] = get_software_version('ims2-cmd-interface')
     meter['software']['mender'] = get_software_version('mender')
 
     # Json to standard out.
-    # print(dumps(meter))
+    print(dumps(meter))
 
-    # Csv to standard out.
-    csv_columns = csv_header_from_nested_dict(meter)
-    row_data = csv_row_from_nested_dict(meter)
-    csv_file = sys_stdout
+    # Csv to standard out (using csv module).
+    # csv_columns = csv_header_from_nested_dict(meter)
+    # row_data = csv_row_from_nested_dict(meter)
+    # csv_file = sys_stdout
+    # try:
+    #     writer = csv.DictWriter(csv_file, fieldnames=csv_columns)
+    #     writer.writeheader()
+    #     writer.writerow(row_data)
+    # except IOError:
+    #     print("I/O error")
 
-    try:
-        writer = csv.DictWriter(csv_file, fieldnames=csv_columns)
-        writer.writeheader()
-        writer.writerow(row_data)
-    except IOError:
-        print("I/O error")
+    # # Csv to standard out (simple txt).
+    # rw = csv_row_from_nested_dict(meter)
+    # for key, value in rw.items():
+    #     print('{}, '.format(value), end = '')
+    # print('')
+
+    # /bin/ps | /bin/grep -i '.*/[i]ms2_cmd_interface.py'  use eithshell=true for pipes?
+    # /bin/ps | /bin/grep -i '.*/[r]d_accum'
+    # /bin/ps | /bin/grep -i '.*[u]ploader'
